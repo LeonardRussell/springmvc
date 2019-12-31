6375a1e4de74635a15aea57e1e4327e634e4b2aa
leonardrussell <zhangyh886@iCloud.com>
2019-12-31 16:43:17 +0800

    11

---
+++ src/main/java/com/cqktzy/entity/User.java
@@ -0,0 +1,41 @@
+package com.cqktzy.entity;
+
+public class User {
+
+    private Integer id;
+    private String uname;
+    private String pwd;
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
